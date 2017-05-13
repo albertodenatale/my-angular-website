7ab1fdfa5b7f77f650667849acb23baf816c0ff0
Alberto De Natale <albertodenatale@Albertos-MBP.home>
2017-05-13 13:04:42 +0100

    removed div and styled component directly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
-import { Component} from '@angular/core';
+import { Component, trigger, state, style, transition, animate } from '@angular/core';
+import { TimerObservable } from "rxjs/observable/TimerObservable";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  animations: [
+    trigger("loaded",
+      [
+        state("loading", style({ transform: "translate(400px)" })),
+        state("loaded", style({ transform: "translate(0)" })),
+        transition('loading => loaded', animate('300ms ease-in'))
+      ])
+  ]
 })
 export class AppComponent {
+  loaded: string = "loading";
 
   ngOnInit() {
+    TimerObservable.create(2000).subscribe(_ => this.loaded="loaded")
   }
 }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit, trigger, state, style, transition, animate  } from '@angular/core';
-import { TimerObservable } from "rxjs/observable/TimerObservable";
 
 @Component({
   selector: 'search',
   template:`
-    <div class="venus" [@loaded]="loaded">
       <!--<button class="btn btn-primary pdf" ><i class="fa fa-file-pdf-o" aria-hidden="true"></i></button> -->
       <toggable>C#</toggable>
       <toggable>.NET</toggable>
@@ -19,25 +17,14 @@ import { TimerObservable } from "rxjs/observable/TimerObservable";
         <span class="input-group-btn">
           <button class="btn btn-primary" type="button"><i class="fa fa-plus" aria-hidden="true"></i></button>
         </span>
-      </div>-->
-    </div>`,
-  animations: [
-    trigger("loaded",
-      [
-        state("loading", style({ transform: "translate(400px)" })),
-        state("loaded", style({ transform: "translate(0)" })),
-        transition('loading => loaded', animate('300ms ease-in'))
-      ])
-  ]
+      </div>-->`
 })
 export class SearchComponent implements OnInit {
 
-  loaded: string = "loading";
 
   constructor() { }
 
   ngOnInit() {
-    TimerObservable.create(2000).subscribe(_ => this.loaded="loaded")
   }
 
 }
