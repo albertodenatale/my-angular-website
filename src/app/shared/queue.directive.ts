5ac262d3b3b99269d021c3e605934b099162d6a1
Alberto De Natale <albertodenatale@Albertos-MBP.home>
2017-05-28 09:41:25 +0100

    fixed detection issue

--- src/app/shared/queue.directive.ts
+++ src/app/shared/queue.directive.ts
@@ -1,8 +1,9 @@
 import { ToggableComponent } from './toggable.component';
 import { TagService } from './../core/tag.service';
 import { Tags, Action } from '../core/tags';
-import { Directive, Input, ViewChildren, QueryList, ContentChildren } from '@angular/core';
+import { Directive, Input, ViewChildren, QueryList, ContentChildren, ChangeDetectorRef } from '@angular/core';
 import { Node } from '../navigation/navigation';
+import { Subscription } from "rxjs/Rx";
 
 @Directive({
   selector: '[queue], queue'
@@ -13,32 +14,43 @@ export class QueueDirective {
   source: Array<Node>
 
   @Input()
-  queue:Array<Node>
+  queue: Array<Node>
 
   @ContentChildren(ToggableComponent)
   toggables: QueryList<ToggableComponent>;
 
-  constructor(private tagService: TagService) {
+  constructor(private tagService: TagService, private changeDetectionRef: ChangeDetectorRef) {
     this.tagService.tagSource.subscribe(
       (t: Tags) => {
         let selected: Node = this.source.find(n => {
           let intersection = t.tags.filter(r => n.path.indexOf(r) > -1);
 
-          return intersection.length == n.path.length &&  intersection.length == t.tags.length;
+          return intersection.length == n.path.length && intersection.length == t.tags.length;
         });
 
         if (selected) {
           this.addToQueueWithIncrementalDelay([selected], t.action);
-          if(this.queue) this.queue.push(selected);
+          if (this.queue) this.queue.push(selected);
         }
       }
     )
   }
 
+  subscriptions: Array<Subscription> = new Array<Subscription>();
+
   private addToQueueWithIncrementalDelay(subnav: Node[], action: Action) {
     //let interval: number = action == Action.Add ? 200 : 200 * subnav.length;
 
-    subnav.forEach(t => this.toggables.map( list => this.toggle(list, t)));
+    this.subscriptions.forEach(s => s.unsubscribe());
+
+    subnav.forEach(t => {
+      if (this.toggle(this.toggables, t) === false) {
+        this.subscriptions.push(this.toggables.changes.subscribe(list => {
+          this.toggle(list, t);
+          this.changeDetectionRef.detectChanges();
+        }));
+      }
+    });
 
     //{
     //   setTimeout(() => {
@@ -59,19 +71,23 @@ export class QueueDirective {
     // }
   }
 
-  private toggle(list: ToggableComponent, node: Node) {
+  private toggle(list: QueryList<ToggableComponent>, node: Node): boolean {
     //this.queue.push(node);
-    let selected = list.id === node.key;
+    let selected: ToggableComponent = this.toggables.find(t => t.id === node.key);
 
-    if (selected) {
-      list.isOn = true;
+    if (selected == null) {
+      return false;
     }
+
+    selected.isOn = !selected.isOn;
+    
+    return true;
   }
 
   ngAfterViewInit() {
     this.toggables.changes.subscribe(x => console.log(x));
   }
-  
+
   // private remove(node: Node) {
   //   let index = this.queue.indexOf(node);
 
