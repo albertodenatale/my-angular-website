1249b675990ac2dfbd183fc7486f57886a475e0b
Alberto De Natale <albertodenatale@Albertos-MBP.home>
2017-06-25 16:55:57 +0100

    improved filtering

--- src/app/experiences/history.component.ts
+++ src/app/experiences/history.component.ts
@@ -39,31 +39,115 @@ export class HistoryComponent implements OnInit {
     let skill = tag.tags[tag.tags.length - 1];
 
     if (tag.action === Action.Add) {
-      let isFilter: boolean = false;
+      let isNarrow: boolean = false;
 
-      this.activePaths.forEach(path => {
+      let paths = this.activePaths.filter(path => {
         let intersection = tag.tags.filter(t => path.indexOf(t) > -1);
 
         if (intersection.length > 0) {
-          path.push(skill);
-          isFilter = true;
+          isNarrow = true;
+
+          return true;
         }
+
+        return false;
       });
 
-      if (isFilter) {
-        this.queue = this.queue.filter(element => element.path.indexOf(skill) > -1);
+      if (paths.length > 1) {
+        throw Error("Only one path at the time can be added");
+      }
+      
+
+      if (isNarrow) {
+        let path = paths[0];
+        for (var index = 0; index < tag.tags.length; index++) {
+          var element = tag.tags[index];
+          
+          if(path.indexOf(element) === -1){
+            path.push(element);
+          }
+        }
+        this.narrow(skill);
       }
       else {
-        this.activePaths.push([skill]);
-        let toAdd = this.experiences.filter(e => e.path.indexOf(skill) > -1);
+        this.addPath([skill]);
+        this.expand(skill);
+      }
+    }
+    else {
+      let isExpand = false;
+      let removed = [];
 
-        toAdd.forEach(element => {
-          this.queue.push(element);
-        });
+      let paths = this.activePaths.filter(path => {
+        let intersection = tag.tags.filter(t => path.indexOf(t) > -1);
+
+        if (intersection.length === tag.tags.length) {
+          let start = path.indexOf(skill);
+
+          if (start > 0) {
+            isExpand = true;
+            removed = path.splice(start, path.length);
+          }
+          return true;
+        }
+        return false;
+      });
+
+      if (paths.length != 1) {
+        throw Error("One path should be selected");
       }
 
+      let path = paths[0];
+
+      if (isExpand) {
+        this.expand(path[path.length - 1]);
+      }
+      else {
+        this.removePath(path);
+      }
     }
   }
+
+  private narrow(skill) {
+    this.queue = this.queue.filter(element => element.path.indexOf(skill) > -1);
+  }
+
+  private expand(skill: string) {
+    let toAdd = this.experiences.filter(e => e.path.indexOf(skill) > -1).filter(f => this.queue.filter(ve => ve.id === f.id).length === 0);
+
+    toAdd.forEach(element => {
+      this.queue.push(element);
+    });
+  }
+
+  private addPath(path: string[]) {
+    this.activePaths.push(path);
+  }
+
+  private removePath(path: string[]) {
+    let toRemove =
+      this.queue
+        .filter(e => e.path.indexOf(path[0]) > -1)
+        .filter(element => {
+          this.activePaths.forEach(activePath => {
+            let intersection = activePath.filter(s => element.path.indexOf(s) > -1);
+
+            if (intersection && intersection.length > 0) {
+              return false;
+            }
+          });
+
+          return true;
+
+        });
+
+    toRemove.forEach(element => {
+      this.remove(this.queue, element);
+    });
+
+    this.remove(this.activePaths, path);
+  }
+
 
   private remove<T>(list: T[], node: T): T[] {
     let index = list.indexOf(node);
