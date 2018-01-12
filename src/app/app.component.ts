80cbe7300f8f261e116acb948d0bfd7a9408a751
Alberto De Natale <albertodenatale@Albertos-MacBook-Pro.local>
2018-01-12 08:07:07 +0000

    refactored animation, changed secondary navigation layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,38 +10,19 @@ import { TimerObservable } from "rxjs/observable/TimerObservable";
 import { Store } from "@ngrx/store";
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
-import { style, trigger, state, transition, animate, keyframes, query, stagger } from "@angular/animations";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  animations: [
-    trigger("loaded",
-      [
-        transition('* => *', [
-          query(':enter', style({ opacity: 0 }), { optional: true }),
-          query(':enter',
-            stagger('200ms', [
-              animate(300,
-                keyframes([
-                  style({ transform: 'translateX(-100%)', opacity: 0 }),
-                  style({ transform: 'translateX(30px)', opacity: 1 }),
-                  style({ transform: 'translateX(0)' })
-                ])
-              )
-            ]), { optional: true }
-          )
-        ])
-      ])
-  ]
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  loaded: string = "loading";
   user: any
   //@HostBinding('@.disabled') public animationsDisabled = true;
 
-  constructor(private store: Store<AppState>, private stateService: StateService, private firebaseAuth: AngularFireAuth, private queryStringComponent:QueryStringService) {
-    this.store.select<ISkillTree>(state => state.navigation).subscribe((skillTree) => { if (skillTree.isLoaded) { this.loaded = "loaded" } });
+  constructor(private store: Store<AppState>, 
+    private stateService: StateService, 
+    private firebaseAuth: AngularFireAuth, 
+    private queryStringComponent:QueryStringService) {
   }
 
   ngOnInit() {
--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -5,24 +5,47 @@ import { Node } from './navigation';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Subject } from "rxjs/Rx";
 import { ISkillTree, getByNavigationBarId, Skill } from "app/shared/skilltree";
-import {Router, ActivatedRoute, Params} from '@angular/router';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { style, trigger, state, transition, animate, keyframes, query, stagger } from "@angular/animations";
 
 @Component({
   selector: 'navigation',
   template: `
-      <toggable *ngFor="let nav of navs" [isOn]="nav.isActive" (whenOff)="whenOff(nav)" (whenOn)="whenOn(nav)">{{nav.label}}</toggable>
-      <subnavigation></subnavigation>`
+  <div [@loaded]="loaded">
+    <toggable *ngFor="let nav of navs" [isOn]="nav.isActive" (whenOff)="whenOff(nav)" (whenOn)="whenOn(nav)">{{nav.label}}</toggable>
+  </div>`,
+  animations: [
+    trigger("loaded",
+      [
+        transition('* => *', [
+          query(':enter', style({ opacity: 0 }), { optional: true }),
+          query(':enter',
+            stagger('200ms', [
+              animate(300,
+                keyframes([
+                  style({ transform: 'translateX(-100%)', opacity: 0 }),
+                  style({ transform: 'translateX(30px)', opacity: 1 }),
+                  style({ transform: 'translateX(0)' })
+                ])
+              )
+            ]), { optional: true }
+          )
+        ])
+      ])
+  ]
 })
 export class NavigationComponent {
 
   navs: Array<Skill>;
-  
+  loaded: string = "loading";
+
   constructor(private store: Store<AppState>) {
     this.store.select<ISkillTree>(state => state.navigation).subscribe(
       skillTree => {
         this.navs = getByNavigationBarId(skillTree, MAINNAV);
+        if (skillTree.isLoaded) { this.loaded = "loaded" }
       }
-    ) 
+    )
   }
 
   whenOn(skill: Skill) {
